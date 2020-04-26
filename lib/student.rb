ba8fe680675246a2200a0a5fd74c67f27a8be503
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-04-25 22:47:40 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+    
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(tid_bit)
+        @knowledge << tid_bit
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
